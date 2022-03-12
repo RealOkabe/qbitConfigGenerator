ca02fde27f5fc36d7b11b078aea6a0053dd34fde
RealOkabe <anmolzagrawal@gmail.com>
2022-03-13 00:48:52 +0530

    Added function to create folders
    Will make folders according to anime names

--- qbitConfigGenerator.py
+++ qbitConfigGenerator.py
@@ -1,4 +1,6 @@
-downloadFolder = input("Enter the path to torrent download folder")
+import os
+
+downloadFolder = input("Enter the path to torrent download folder: ")
 
 # Function to read anime names from file called animenames.txt
 # Just need to make a txt file containing anime titles separated by \n
@@ -10,4 +12,12 @@ def readAnimeNames():
     animeNamesFile = open('animenames.txt', 'r')
     animeNamesData = animeNamesFile.read()
     animeNamesList = animeNamesData.split('\n')
+    animeNamesFile.close()
     return animeNamesList
+
+# Function to create the anime name folders
+def createAnimeFolders():
+    animeNamesList = readAnimeNames()
+    for animeName in animeNamesList:
+        fullPath = os.path.join(downloadFolder, animeName)
+        os.makedirs(fullPath, exist_ok = True)
